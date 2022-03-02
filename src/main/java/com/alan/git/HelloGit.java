8e7de280fd596ebdd9f187571ac5a96ec8eb0443
Alan <cxy814941789@gmail.com>
2022-03-03 03:58:11 +0800

    更新1

--- src/main/java/com/alan/git/HelloGit.java
+++ src/main/java/com/alan/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.alan.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git!更新1");
     }
 }
